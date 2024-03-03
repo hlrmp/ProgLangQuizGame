e2fe66b5098ca25cdcf24a21ab07c7aff9407ac8
hlrmp <haysjhune@gmail.com>
2024-03-03 13:33:47 +0800

    Add project files.

---
+++ ProgLangQuizGame/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+using System.Text.RegularExpressions;
+using ProgLangQuizGame;
+
+public class gameOne
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Hello, World!");
+        string n = Console.ReadLine();
+
+        Console.WriteLine(n);
+        try
+        {
+            if (string.IsNullOrEmpty(n))
+            {
+
+            }
+            else
+            {
+               
+            }
+
+
+        }
+        catch (NumberFormat e)
+        {
+            Console.WriteLine("Number Format Exception " + e.Message);
+        }
+
+       
+
+
+       
+
+
+    }
+
+
+    exceptionClass ec = new exceptionClass(); // exceptionClass Instance
+
+
+    class nullExceptiom : Exception
+    {
+        public nullExceptiom(string str) : base(str) { }
+    }
+    class StringFormatException : Exception
+    {
+        public StringFormatException(string str) : base(str) { }
+    }
+    class NumberFormat : Exception
+    {
+        public NumberFormat(string str) : base(str) { }
+    }
+
+
+
+    public string Answer(string answer)
+    {
+        if (Regex.IsMatch(answer, exceptionClass.letters))
+        {
+
+            return answer;
+
+        }
+
+        else
+        {
+            throw new StringFormatException("please enter your First Name Correctly");
+        }
+
+
+    }
+}
+
+
---
+++ ProgLangQuizGame/exceptionClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgLangQuizGame
+{
+    internal class exceptionClass
+    {
+
+        public static string letters = @"^[a-zA-Z][a-zA-Z\\s]+$";
+        public static string numbers = @"^09[0-9]{9}$";
+        public static string age = @"^[0-9]{1,3}$";
+        public static string schar = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
+        public static string mix = @"^[A-Za-z0-9]";
+        public static string Intg = @"^[0-9]+$";
+        public static string doub = @"^[0-9]+[.[0-9]{0,2}]?";
+
+
+      
+
+    }
+}
