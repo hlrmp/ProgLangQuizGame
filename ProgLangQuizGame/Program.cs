c3e4336e972694e7f9da562ff9c327927b9dd243
hlrmp <haysjhune@gmail.com>
2024-03-04 23:56:24 +0800

    1

--- ProgLangQuizGame/Program.cs
+++ ProgLangQuizGame/Program.cs
@@ -2,47 +2,86 @@
 
 using System.Collections;
 using System.Text.RegularExpressions;
+using System.Xml.Schema;
 using ProgLangQuizGame;
 
 public class gameOne
 {
     public static void Main(string[]args)
     {
-        Console.WriteLine("Hello, World!");
-        int n = int.Parse(Console.ReadLine());
-        int ans = 0;
+     
 
 
         string[,]questions = new string[,]
         {
             {"1. It is an event that occurs during the execution of a program that disrupts the normal flow of instructions ? ",
-                "a. Exception     b. Extends     c. Inheritance",}
-
-
+                "a. Exception     b. Extends     c. Inheritance",
+            }
 
         };
-        string[] answers = new string []
+        string[,] answers = new string [,]
         {
-
+            {"a","b"}
         };
 
+        string[] status = new string[10];
+        
+
+        
 
 
-        int a = 0;
+
+        int a = 0; // total number of Questions
         int c = 0; // correct
         int w = 0; // wrong
 
+        
+
         do
         {
-            Console.WriteLine(questions[a,0] + questions[a,1]);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("_______________________________");
+            Console.ResetColor();
+
+            Console.WriteLine(questions[a, 0] + questions[a, 1]);
+            string ans = Console.ReadLine();
+
+            status[a] = (ans);
+
+            if ((answers[0,a]).Equals(status[a]))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Correct");
+                Console.ResetColor();
+
+                c++;
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+
+                w++;
+            }
+
+            a++;
 
         }
-        while (a < 10);
+        while (a < 1);
         {
 
-            Console.WriteLine("ans");
+            Console.Write("\u001b[1m");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("    Your Score: " + c + " / " +a + "          ");
+            Console.WriteLine("-------------------------------");
+            Console.ResetColor();
+            //  Console.WriteLine(a);
+
 
         }
+
 
         try
         {
@@ -82,22 +121,9 @@ public class gameOne
 
 
 
-    public string Answer(string answer)
-    {
-        if (Regex.IsMatch(answer, exceptionClass.letters))
-        {
-
-            return answer;
-
-        }
-
-        else
-        {
-            throw new StringFormatException("please enter your First Name Correctly");
-        }
-
-
-    }
 }
+
+
+
 
 
